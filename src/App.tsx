3323183c5100f5d96635f853447b0f87771704e0
Sergey <santerakis@gmail.com>
2022-12-30 22:53:33 +0200

    reducer counter without limit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import React, {useState} from 'react';
+import React, {useReducer, useState} from 'react';
 import './App.css';
 
+const counterReducer = (counter: number, action: any) => {
+    switch (action.type) {
+        case 'add':
+            return counter + 1
+        case  'res':
+            return 0
+        default:
+            return  counter
+    }
+}
+
 function App() {
-    const [num, setNum] = useState(0)
-    const incNumHandler = () => num < 5 && setNum(num + 1)
-    const resNumHandler = () => setNum(0)
+    const [num, dispatchNum] = useReducer(counterReducer, 0)  //применили другой инструмент(useReducer) стэйт менеджмента
+    const incNumHandler = () => dispatchNum({type: 'add'})
+    const resNumHandler = () => dispatchNum({type: 'res'})
+    // const [num, setNum] = useState(0)
+    // const incNumHandler = () => num < 5 && setNum(num + 1)
+    // const resNumHandler = () => setNum(0)
     return (
         <div className="App">
             <div>{num}</div>
